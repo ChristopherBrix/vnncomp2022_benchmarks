dd2b2ba25aba542541e0733112fabdcce1c7d107
Christopher Brix <Christopher.Brix@rwth-aachen.de>
2022-06-24 01:10:29 +0200

    Fixed handling of external resources

--- benchmarks/nn4sys/additional_downloads.sh
+++
@@ -1,1 +0,0 @@
-wget https://rwth-aachen.sciebo.de/remote.php/webdav/vnncomp2022/nn4sys/onnx/mscn_2048d_dual.onnx.gz nn4sys/onnx/mscn_2048d_dual.onnx.gz  
--- setup.sh
+++ setup.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
 
-gunzip vnncomp2022_benchmarks/benchmarks/*/onnx/* vnncomp2022_benchmarks/benchmarks/*/vnnlib/*
-wget https://github.com/onnx/models/raw/main/vision/classification/vgg/model/vgg16-7.onnx benchmarks/vggnet16_2022/onnx/vgg16-7.onnx
+wget https://rwth-aachen.sciebo.de/s/1z7n1nh0YVT9KjK/download -O large_models.zip
+unzip large_models.zip -d large_models
+cd large_models/vnncomp2022/
+find . -type f -exec mv "{}" "../../benchmarks/{}" \;
+cd ../..
+rm -r large_models large_models.zip 
+
+gunzip benchmarks/*/onnx/* benchmarks/*/vnnlib/*
+wget https://github.com/onnx/models/raw/main/vision/classification/vgg/model/vgg16-7.onnx -O benchmarks/vggnet16_2022/onnx/vgg16-7.onnx
