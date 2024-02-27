57591aef439056bdc3fd976aaab4d14d94c71220
Zhouxing Shi <zhouxingshichn@gmail.com>
2024-02-26 17:12:02 -0800

    Fix the link to VGG16-7

--- setup.sh
+++ setup.sh
@@ -8,4 +8,4 @@ cd ../..
 rm -r large_models large_models.zip 
 
 gunzip benchmarks/*/onnx/* benchmarks/*/vnnlib/*
-wget https://github.com/onnx/models/raw/main/vision/classification/vgg/model/vgg16-7.onnx -O benchmarks/vggnet16_2022/onnx/vgg16-7.onnx
+wget https://github.com/onnx/models/raw/5faef4c33eba0395177850e1e31c4a6a9e634c82/vision/classification/vgg/model/vgg16-7.onnx -O benchmarks/vggnet16_2022/onnx/vgg16-7.onnx
